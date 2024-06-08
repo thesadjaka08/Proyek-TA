e78d212444559c771f243e46b07008f04d088f45
dantyputri2 <154827485+dantyputri2@users.noreply.github.com>
2024-06-08 12:07:04 +0700

    Update pinjaman.py

--- pages/pinjaman.py
+++ pages/pinjaman.py
@@ -3,6 +3,28 @@ import pandas as pd
 import joblib
 from xgboost import XGBClassifier
 
+
+# st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
+no_sidebar_style = """
+    <style>
+        div[data-testid="stSidebarNav"] {display: none;}
+    </style>
+"""
+st.markdown(no_sidebar_style, unsafe_allow_html=True)
+
+# sidebars
+st.sidebar.header("Menu")
+st.sidebar.page_link("pages/Homepage.py", label="Beranda")
+st.sidebar.page_link("https://chatloanbot.000webhostapp.com/", label="Chatbot")
+st.sidebar.page_link("pages/info.py", label="Info")
+st.sidebar.markdown("""
+<a href="Login.py" target="_self">
+    <button style="background-color:#f63366;color:white;border:none;padding:10px 20px;border-radius:5px; margin-top:50px;">
+        Logout
+    </button>
+</a>
+""", unsafe_allow_html=True)
+
 # Load the trained model
 model = joblib.load('Xgboost_model.sav', 'rb')
 
@@ -10,19 +32,36 @@ model = joblib.load('Xgboost_model.sav', 'rb')
 st.title("Prediksi Kelayakan Pinjaman")
 
 # Collect user input
-st.page_link("pages/info.py", label="Info")
+if st.button("Info"):
+    st.switch_page("pages/info.py")
 
-Age = st.number_input("Masukkan Usia", min_value=10, max_value=100)
+Age = st.number_input("Masukkan Usia", min_value=17, max_value=100)
+
 Experience = st.number_input("Jumlah Pengalaman", min_value=1, max_value=100)
+
 Income = st.number_input("Jumlah Pendapatan", min_value=0.0, max_value=20.0)
+
 Family = st.selectbox("Jumlah Keluarga", options=[1, 2, 3, 4])
+
 CCAvg = st.number_input("Jumlah CCAvg", min_value=0.0, max_value=10.0)
+
 Education = st.selectbox("Pendidikan", options=[1, 2, 3])
+st.caption(":red[**1**]: Sarjana (Bachelor's degree) :red[**2**]: Magister (Master's degree) :red[**3**]: Profesional (Professional degree)")
+
 Mortgage = st.number_input("Jumlah Mortgage/Hipotek", min_value=0, max_value=650)
+
 Securitiesaccount = st.selectbox("Apakah memiliki Securities Account", options=[0, 1])
+st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
+
+
 CDaccount = st.selectbox("Apakah memiliki CD Account", options=[0, 1])
+st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
+
 Online = st.selectbox("Apakah Online", options=[0, 1])
+st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
+
 CreditCard = st.selectbox("Apakah memiliki CreditCard", options=[0, 1])
+st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
 
 # Create a DataFrame from user input
 input_data = pd.DataFrame({
