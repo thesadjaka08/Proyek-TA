23e45cc23fceb550e3148360e99e74512c07c0d6
dantyputri2 <154827485+dantyputri2@users.noreply.github.com>
2024-06-19 17:37:38 +0700

    Update Login.py

--- Login.py
+++ Login.py
@@ -177,10 +177,10 @@ def main():
                 else:
                     st.error("Incorrect verification code")
 
-    elif st.session_state.page == 'mainapp':
+    elif st.session_state.page == 'homepage':
         # Execute the code from homepage.py
-        with open("pages/mainapp.py") as f:
-            code = compile(f.read(), "pages/mainapp.py", 'exec')
+        with open("pages/Homepage.py") as f:
+            code = compile(f.read(), "pages/Homepage.py", 'exec')
             exec(code, globals())
 
 if __name__ == "__main__":
