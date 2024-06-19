4ffeb90c481a66775ba88d1e6e0c0d5b12bbd912
dantyputri2 <154827485+dantyputri2@users.noreply.github.com>
2024-06-19 17:26:56 +0700

    Create main.py

---
+++ pages/main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from homepage import homepage
+from pinjaman import pinjaman
+from info import info
+
+def main():
+    st.sidebar.title("Menu")
+    page = st.sidebar.selectbox("Select a page", ["Homepage", "Prediksi Kelayakan Pinjaman", "Info"])
+
+    if page == "Homepage":
+        homepage()
+    elif page == "Prediksi Kelayakan Pinjaman":
+        pinjaman()
+    elif page == "Info":
+        info()
+
+if __name__ == "__main__":
+    main()
