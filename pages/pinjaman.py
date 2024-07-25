5dce809139f1e020404cdfadb58bd32316e506b9
thesadjaka08 <127266628+thesadjaka08@users.noreply.github.com>
2024-07-25 16:02:56 +0700

    Update pinjaman.py

--- pages/pinjaman.py
+++ pages/pinjaman.py
@@ -43,9 +43,9 @@ Income = st.number_input("Jumlah Pendapatan Bulanan (dalam Rupiah)", min_value=0
 CCAvg = st.number_input("Jumlah Pengeluaran Bulanan (dalam Rupiah)", min_value=0, max_value=100000000)
 Mortgage = st.number_input("Jumlah Hipotek (dalam Rupiah)", min_value=0, max_value=100000000)
 st.caption("Nilai barang yang dijadikan jaminan nasabah kepada bank")
-Securities Account = st.selectbox("Apakah memiliki Rekening Investasi?", options=[0, 1])
+SecuritiesAccount = st.selectbox("Apakah memiliki Rekening Investasi?", options=[0, 1])
 st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
-CD Account = st.selectbox("Apakah memiliki Rekening CD (Sertifikat Deposito)?", options=[0, 1])
+CDAccount = st.selectbox("Apakah memiliki Rekening CD (Sertifikat Deposito)?", options=[0, 1])
 st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
 Online = st.selectbox("Apakah menggunakan layanan Mobile Banking?", options=[0, 1])
 st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
@@ -61,8 +61,8 @@ input_data = pd.DataFrame({
     'Income': [Income],
     'CCAvg': [CCAvg],
     'Mortgage': [Mortgage],
-    'Securities Account': [Securities Account],
-    'CD Account': [CD Account],
+    'SecuritiesAccount': [SecuritiesAccount],
+    'CDAccount': [CDAccount],
     'Online': [Online],
     'CreditCard': [CreditCard]
 })
