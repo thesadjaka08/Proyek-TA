54eb014c1b8b1a05a3ebaf4fe146c4b2a56c97ca
thesadjaka08 <127266628+thesadjaka08@users.noreply.github.com>
2024-07-25 16:23:57 +0700

    Update pinjaman.py

--- pages/pinjaman.py
+++ pages/pinjaman.py
@@ -35,20 +35,30 @@ if st.button("Info"):
     st.switch_page("pages/info.py")
 
 Age = st.number_input("Usia", min_value=18, max_value=100)
+
 Experience = st.number_input("Jumlah Tahun Pengalaman Kerja", min_value=1, max_value=100)
+
 Family = st.number_input("Jumlah Keluarga", min_value=1, max_value=50)
+
 Education = st.selectbox("Pendidikan Terakhir", options=["1", "2", "3"])
 st.caption(":red[**1**]: Sarjana (Bachelor's degree) :red[**2**]: Magister (Master's degree) :red[**3**]: Profesional (Professional degree)")
+
 Securities_Account = st.selectbox("Apakah memiliki Rekening Investasi?", options=[0, 1])
 st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
+
 CD_Account = st.selectbox("Apakah memiliki Rekening CD (Sertifikat Deposito)?", options=[0, 1])
 st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
+
 Online = st.selectbox("Apakah menggunakan layanan Mobile Banking?", options=[0, 1])
 st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
+
 CreditCard = st.selectbox("Apakah memiliki kartu kredit?", options=[0, 1])
 st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
+
 Income = st.number_input("Jumlah Pendapatan Bulanan (dalam Rupiah)", min_value=0, max_value=500000000)
+
 CCAvg = st.number_input("Jumlah Pengeluaran Bulanan (dalam Rupiah)", min_value=0, max_value=100000000)
+
 Mortgage = st.number_input("Jumlah Hipotek (dalam Rupiah)", min_value=0, max_value=100000000)
 st.caption("Nilai barang yang dijadikan jaminan nasabah kepada bank")
 
@@ -61,10 +71,10 @@ input_data = pd.DataFrame({
     'Securities Account': [Securities_Account],
     'CD Account': [CD_Account],
     'Online': [Online],
-    'CreditCard': [CreditCard]
+    'CreditCard': [CreditCard],
     'Income': [Income],
     'CCAvg': [CCAvg],
-    'Mortgage': [Mortgage],
+    'Mortgage': [Mortgage]
 })
 
 # Ensure all data types are numeric and there are no missing values
