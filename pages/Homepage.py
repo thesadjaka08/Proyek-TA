dc49cf4ff2658c2eb526692b01bd8a3c9197a8b4
thesadjaka08 <127266628+thesadjaka08@users.noreply.github.com>
2024-09-27 00:01:34 +0700

    Homepage.py

--- pages/Homepage.py
+++ pages/Homepage.py
@@ -1,4 +1,3 @@
-# homepage.py
 import streamlit as st
 
 def homepage():
@@ -7,7 +6,7 @@ def homepage():
 
     st.sidebar.image('images/logo.jpg')
 
-        # Set the title of the homepage
+    # Set the title of the homepage
     st.markdown('<div style="text-align:center; font-size:46px; font-weight:bold; margin-bottom:25px;">Selamat datang di Sistem Prediksi Kelayakan Pinjaman kami!</div>', unsafe_allow_html=True)
 
     # sidebars
@@ -22,41 +21,38 @@ def homepage():
     </a>
     """, unsafe_allow_html=True)
 
-
     # Create columns
     col1, col2 = st.columns([1, 2])
 
     with col1:
         # Add an image
-        st.image('images/chatbot.png', use_column_width=True)
-
+        st.image('images/logo.jpg', use_column_width=True)
 
     with col2:
         # Add some text
-        st.markdown('<div style="text-align:justify; margin-top:40px;">Platform ini dirancang khusus untuk staf bank guna mempermudah proses pengajuan pinjaman. Dengan teknologi prediksi canggih dan dukungan chatbot interaktif, kami membantu Anda dalam mengevaluasi kelayakan pinjaman dengan lebih efisien dan akurat. Mari tingkatkan produktivitas dan pelayanan dengan alat inovatif kami!</div>', unsafe_allow_html=True)
+        st.markdown('<div style="text-align:justify; margin-top:40px;">Sistem ini dirancang khusus untuk lembaga pemberi pinjaman pribadi guna mempermudah proses pengajuan pinjaman. Dengan teknologi prediksi canggih, kami membantu mengevaluasi kelayakan pinjaman dengan lebih cepat dan akurat. Tingkatkan produktivitas dan pelayanan Anda dengan solusi inovatif kami!</div>', unsafe_allow_html=True)
 
     # Add a header
     st.markdown('<div style="text-align:center; font-size:30px; font-weight:bold; margin-bottom:25px; margin-top:35px;">Prediksi Akurat, Proses Cepat, Keputusan Tepat</div>', unsafe_allow_html=True)
 
-
     # Create columns
     col1, col2 = st.columns([1, 1])
+
     with col1:
         st.image('images/predictive.png', use_column_width=True)
         st.markdown('<div style="text-align:center; font-size:20px; font-weight:bold; margin-bottom:18px;">Prediksi Kelayakan Pinjaman</div>', unsafe_allow_html=True)
-        st.markdown('<div style="text-align:center">Sistem menganalisis data yang dimasukkan menggunakan model machine learning yang telah dilatih sebelumnya untuk memprediksi kelayakan pinjaman.</div>', unsafe_allow_html=True)
+        st.markdown('<div style="text-align:center">Sistem ini menganalisis data yang diinput menggunakan model machine learning yang sudah dilatih sebelumnya untuk memprediksi kelayakan pinjaman secara akurat.</div>', unsafe_allow_html=True)
 
     with col2:
         st.image('images/analytics.png', use_column_width=True)
-        st.markdown('<div style="text-align:center; font-size:20px; font-weight:bold; margin-bottom:25px; margin-top:25px;">Pelaporan dan Analisis</div>', unsafe_allow_html=True)
-        st.markdown('<div style="text-align:center">Menghasilkan laporan periodik dan analisis mendalam mengenai tren pengajuan pinjaman dan kinerja model prediksi.</div>', unsafe_allow_html=True)
+        st.markdown('<div style="text-align:center; font-size:20px; font-weight:bold; margin-bottom:25px; margin-top:25px;">Berbagi Informasi</div>', unsafe_allow_html=True)
+        st.markdown('<div style="text-align:center">Memberikan informasi lebih detail tentang fitur yang digunakan untuk prediksi.</div>', unsafe_allow_html=True)
 
     # Add a header
     st.markdown('<div style="text-align:center; font-size:30px; font-weight:bold; margin-bottom:25px; margin-top:35px;">Gunakan Fitur Kami, Raih Hasil Terbaik!</div>', unsafe_allow_html=True)
 
     # Add some text
-    st.markdown('<div style="text-align:center; margin-bottom:25px;">Berikut adalah beberapa fitur utama yang kami tawarkan untuk memudahkan pekerjaan Anda dan meningkatkan efisiensi dalam pengelolaan pinjaman pribadi.</div>', unsafe_allow_html=True)
-
+    st.markdown('<div style="text-align:center; margin-bottom:25px;">Berikut adalah fitur utama yang kami tawarkan untuk memudahkan pekerjaan Anda dan meningkatkan efisiensi dalam pengelolaan pinjaman pribadi.</div>', unsafe_allow_html=True)
 
     # Create columns
     col1, col2 = st.columns([1, 2])
@@ -65,21 +61,10 @@ def homepage():
         # Use st.image to display the image
         st.image('images/technology.png', use_column_width=True)
     
-        # Add some space below the image using st.markdown and HTML
+        # Add some space below the image
         st.markdown('<div style="margin-bottom:25px; width:2px;"></div>', unsafe_allow_html=True)
 
-        # # Add an image
-        # st.image('<div style= margin-bottom:25px;">images/technology.png</div>', use_column_width=True)
-
     with col2:
-        st.markdown('''
-        <div style="display: flex; align-items: center; margin-bottom: 10px; margin-top: 5px; margin-left: 10px;">
-            <span style="font-size: 25px; margin-right: 10px;">🤖</span>
-            <div>Chatbot yang dapat merespons pertanyaan-pertanyaan mengenai proses pengajuan pinjaman</div>
-        </div>
-        ''', unsafe_allow_html=True)
-        st.link_button("Chatbot", "https://chatloanbot.000webhostapp.com/")
-
         st.markdown('''
         <div style="display: flex; align-items: center; margin-bottom: 10px; margin-top: 5px; margin-left: 10px;">
             <span style="font-size: 25px; margin-right: 10px;">📈</span>
@@ -89,8 +74,6 @@ def homepage():
         
         if st.button("Prediksi Kelayakan Pinjaman"):
             st.switch_page("pages/pinjaman.py")
-
-    
 
 if __name__ == "__main__":
     homepage()
